2c70437ccec4fa2929e55a4d2cefe929efe336c6
agustinanahir <agustina.ferreira@live.com>
2022-11-02 22:11:12 -0300

    add function input to see the img

--- script.js
+++ script.js
@@ -11,3 +11,46 @@ btnTheme.addEventListener("click", () => {
     btnTheme.innerText = "Modo oscuro"
   }
 });
+
+let btnImage = document.getElementById("btn-image");
+let btnTxt = document.getElementById("btn-text");
+let asideImage = document.getElementById("section-image");
+let asideTxt = document.getElementById("section-text");
+
+btnTxt.addEventListener("click", (event)=>{
+  asideTxt.classList.remove("display-none");
+  asideImage.classList.add("display-none");
+})
+
+btnImage.addEventListener("click", (event)=>{
+  asideTxt.classList.add("display-none");
+  asideImage.classList.remove("display-none");
+})
+
+
+const inputText = document.getElementById('inputText');
+const renderText = document.getElementById('renderText');
+
+
+inputText.addEventListener('input', (event) => {
+  const textoIngresado = event.target.value;
+  renderText.innerHTML= textoIngresado
+})
+
+const inputBotton=document.getElementById("inputBotton");
+const bottonText = document.getElementById('bottonText');
+
+inputBotton.addEventListener('input', (Event) => {
+  const textoIngresadoB = Event.target.value;
+  bottonText.innerHTML = textoIngresadoB ;
+  
+})
+
+let url = document.getElementById("inputUrl");
+let imageContainer = document.getElementById("img-meme");
+
+
+url.addEventListener ("input", (event)=>{
+imageContainer.style.backgroundImage = `url("${event.target.value}")`;
+  
+})
