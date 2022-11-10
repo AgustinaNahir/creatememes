af52a0e6e42d8c53912a8cf30164e5ba37397a03
agustinanahir <agustina.ferreira@live.com>
2022-11-10 19:24:34 -0300

    add js for input color text and inpur background color

--- script.js
+++ script.js
@@ -16,19 +16,19 @@ btnTheme.addEventListener("click", () => {
 
 /* Botones de Imagen / Texto aside*/
 
-let btnImage = document.getElementById("btn-image");
 let btnTxt = document.getElementById("btn-text");
-let asideImage = document.getElementById("section-image");
+let btnImage = document.getElementById("btn-image");
 let asideTxt = document.getElementById("section-text");
+let asideImage = document.getElementById("section-image");
 
-btnTxt.addEventListener("click", (event)=>{
-  asideTxt.classList.remove("display-none");
-  asideImage.classList.add("display-none");
+btnImage.addEventListener("click", ()=>{
+  asideImage.classList.remove("display-none");
+  asideTxt.classList.add("display-none");
 })
 
-btnImage.addEventListener("click", (event)=>{
-  asideTxt.classList.add("display-none");
-  asideImage.classList.remove("display-none");
+btnTxt.addEventListener("click", ()=>{
+  asideTxt.classList.remove("display-none");
+  asideImage.classList.add("display-none");
 })
 
 /* input textos */
@@ -66,3 +66,36 @@ const downloadMeme = () => {
 };
 
 btnDownload.addEventListener("click", downloadMeme);
+
+/* filtros de imagen*/
+
+let brightnessImg = document.getElementById("brightness");
+let opacityImg = document.getElementById("opacity");
+
+
+/*filtros de texto*/
+let colorText = document.getElementById("color-text");
+
+colorText.addEventListener('blur', (event) =>{
+  let colorText = event.target.value;
+  topText.style.color = colorText;
+})
+
+colorText.addEventListener('blur', (event) =>{
+  let colorText = event.target.value;
+  bottomText.style.color = colorText;
+})
+
+let topTextContainer = document.getElementById("top-text-container");
+let bottomTextContainer = document.getElementById("bottom-text-container");
+let backgroundText = document.getElementById("background-text");
+
+backgroundText.addEventListener('blur', (event) => {
+  let backgroundColorText = event.target.value;
+  topTextContainer.style.backgroundColor = backgroundColorText;
+});
+
+backgroundText.addEventListener('blur', (event) => {
+  let backgroundColorText = event.target.value;
+  bottomTextContainer.style.backgroundColor = backgroundColorText;
+});
